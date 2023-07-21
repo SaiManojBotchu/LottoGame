13d8651d762aa076a0a48080979f143fdeff50c3
Sai Manoj <saimanoj2826@gmail.com>
2023-07-21 21:36:39 +0530

    removed other generate funcs

--- src/Lottery.js
+++ src/Lottery.js
@@ -4,36 +4,30 @@ import './Lottery.css';
 
 class Lottery extends Component {
   static defaultProps = {
-    numBalls : 6,
+    numBalls: 6,
     maxNum: 40
-  }
+  };
 
   constructor(props) {
     super(props);
-    this.state = {nums: Array.from({length: this.props.numBalls})};
+    this.state = { nums: Array.from({ length: this.props.numBalls }) };
     this.generate = this.generate.bind(this);
   }
 
-  // generate() {
-  //   const newArray = this.state.nums.map(n => Math.floor(Math.random() * this.props.maxNum) + 1);
-  //   this.setState({nums : newArray});
-  // }
-  // generate() {
-  //   this.setState(curState => {
-  //     return { nums: curState.nums.map(n => Math.floor(Math.random() * this.props.maxNum) + 1) }
-  //   });
-  // }
   generate() {
-    this.setState(curState => ({ 
-      nums: curState.nums.map(n => Math.floor(Math.random() * this.props.maxNum) + 1)
-    }));
+    // Array.Prototype.map return new array
+    const newArray = this.state.nums.map(n => Math.floor(Math.random() * this.props.maxNum) + 1);
+    this.setState({nums : newArray});
   }
+
   render() {
     return (
-      <div className="Lottery">
+      <div className='Lottery'>
         <h1>{this.props.title}</h1>
         <div>
-          {this.state.nums.map(n => <Ball num={n} />)}  
+          {this.state.nums.map((n, i) => (
+            <Ball key={i} num={n} />
+          ))}
         </div>
         <button onClick={this.generate}>Generate</button>
       </div>
